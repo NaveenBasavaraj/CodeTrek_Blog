2acd038c84a97dabda7eaf318417e62c33518a34
Naveen Basavaraj <naveen.b.workmail@gmail.com>
2025-05-24 16:28:43 +0000

    docker setup

--- CodeTrek/CodeTrek/settings.py
+++ CodeTrek/CodeTrek/settings.py
@@ -11,22 +11,22 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(BASE_DIR / ".env")
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
-
+import os
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = $SECRET_KEY
-#SECRET_KEY = 'django-insecure-@a-*x0gi@=x*#z4^oq5300vp=0ze#uls*7*r!hsrns0y^n50qb'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+DEBUG = os.getenv("DEBUG") == "True"
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
 CORS_ALLOW_ALL_ORIGINS = True
 
 # Application definition
